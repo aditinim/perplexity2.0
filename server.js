1741113584ce68f46f2c1ca965f227e11d8dba3d
Aditi Nim <nimaditi7@gmail.com>
2026-06-28 03:43:15 +0530

    gen ai tools and agents

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import "dotenv/config"
 import app from './src/app.js'
 import connectDB from './src/config/database.js';
-
+import {testAi} from "./src/services/ai.service.js";
 
 const PORT= process.env.PORT || 3000;
 
+testAi();
 
 connectDB()
     .catch((err)=>{
