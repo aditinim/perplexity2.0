6b176048e1f092f2e1c088266f52ec24edaef8bd
Aditi Nim <nimaditi7@gmail.com>
2026-06-28 01:51:01 +0530

    authentication done

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -21,6 +21,12 @@ export async function register(req, res) {
 
     const user = await userModel.create({ username, email, password });
 
+    console.log("JWT_SECRET =", process.env.JWT_SECRET);
+    const emailVerificationToken = jwt.sign({
+        email: user.email,
+
+    }, process.env.JWT_SECRET)
+
     await sendEmail({
         to: email,
         subject: "Welcome to Perplexity2.0",
@@ -49,7 +55,7 @@ export async function register(req, res) {
 
                 <div style="text-align:center;margin:35px 0;">
                     <a
-                    href="http://localhost:5173"
+                    href="http://localhost:3000/api/auth/verify-email?token=${emailVerificationToken}"
                     style="background:#111827;color:#ffffff;text-decoration:none;padding:12px 24px;border-radius:6px;display:inline-block;font-weight:600;"
                     >
                     Get Started
@@ -83,4 +89,115 @@ export async function register(req, res) {
             email: user.email
         }
     });
+}
+
+
+
+
+export async function verifyEmail(req, res) {
+    const { token } = req.query;
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+
+
+
+
+        const user = await userModel.findOne({ email: decoded.email });
+
+
+        if (!user) {
+            return res.status(400).json({
+                message: "Invalid token",
+                success: false,
+                err: "User not found"
+            })
+        }
+
+
+        user.verified = true;
+        await user.save();
+
+
+        const html =    `
+            <h1>Email Verified Successfully</h1>
+            <p>Your email has been verified. You can now log in to your account.</p>
+        `
+
+        return res.send(html)
+    }
+    catch (err) {
+        return res.status(400).json({
+            message: "Invalid or expired token",
+            success: false,
+            err: err.message
+        })
+    }
+
+}
+
+
+export async function login(req, res){
+    const {email, password} = req.body;
+
+    const user= await userModel.findOne({email});
+
+
+    if(!user){
+        return res.status(400).json({
+            message: "Invalid email or password",
+            success: false,
+            err: "Incorrect password"
+        })
+    }
+
+    if(!user.verified){
+        return res.status(400).json({
+            message: "Please verify you email before loggin in",
+            success: false,
+            err: "Email not verified"
+        })
+    }
+
+    const token= jwt.sign({
+        id: user._id,
+        username: user.username,
+
+    }, process.env.JWT_SECRET, {expiresIn: "7d"});
+
+    res.cookie("token", token);
+
+    res.status(200).json({
+        message: "Login successful",
+        success: true,
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+    })
+}
+
+
+export async function getMe(req, res){
+    const userId= req.user.id;
+
+    const user= await userModel.findById(userId).select("-password");
+
+
+    if(!user){
+        return res.status(404).json({
+            message: "User not found",
+            success: false,
+            err: "User not found"
+        })
+    }
+
+    res.status(200).json({
+        message: "User details fetched successfully",
+        success: true,
+        user
+    })
+
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,21 @@
 import {Router } from 'express';
-import { registerValidator } from '../validators/auth.validator.js';
-import { register } from '../controllers/auth.controller.js';
+import { loginValidator, registerValidator } from '../validators/auth.validator.js';
+import { register, verifyEmail, login, getMe } from '../controllers/auth.controller.js';
+import {authUser} from '../middlewares/auth.middleware.js'
+
 
 const authRouter= Router();
 
-authRouter.post("/register", registerValidator, register)
+authRouter.post("/register", registerValidator, register);
+
+authRouter.get("/verify-email", verifyEmail);
+
+authRouter.post("/login", loginValidator, login);
+
+authRouter.get("/get-me", authUser, getMe);
+
+
+
 
 
 export default authRouter
